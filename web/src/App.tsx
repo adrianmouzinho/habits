43636de3b8853e65113e289a827c7fa1a03174e8
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2023-01-21 10:31:50 -0300

    Aulas 01 e 02

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import './styles/global.css'
+import { Header } from './components/Header'
+
+import { HabitDay } from './components/HabitDay'
+import { SummaryTable } from './components/SummaryTable'
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16 items-center">
+
+        <Header />
+        <SummaryTable />
+
+      </div>
+    </div>
+  )
+}
